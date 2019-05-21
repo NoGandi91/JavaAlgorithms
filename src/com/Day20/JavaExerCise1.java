78f37a9285564eb477e7a9f792c11a392d5354ee
NoHong <shghdgusa@naver.com>
2019-05-21 18:32:22 +0900

    5.21

---
+++ src/com/Day20/JavaExerCise1.java
@@ -0,0 +1,20 @@
+package com.Day20;
+
+import java.util.Scanner;
+
+public class JavaExerCise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("È­¾¾¿Âµµ");
+		double f = sc.nextDouble();
+		
+		
+		System.out.println();
+		double x = (5.0/9)*(f-32);		
+		System.out.println("x = "+x);
+	}
+
+}
